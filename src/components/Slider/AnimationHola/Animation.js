0892b046c3b74d94b9f877b05e35e2c1db99ac2f
Paolollch <paolo.llamoza@lilab.pe>
2022-05-29 03:49:55 -0500

    Agregando . a animation

--- src/components/Slider/AnimationHola/Animation.js
+++ src/components/Slider/AnimationHola/Animation.js
@@ -7,10 +7,10 @@ export default function Animation(){
         <div className="wrapper">
         <div className="static-txt">Hola soy</div>
         <ul className="dynamic-txts">
-            <li><span>Paolo</span></li>
-            <li><span>Developer</span></li>
-            <li><span>Front-end</span></li>
-            <li><span>Back-end</span></li>
+            <li><span>Paolo.</span></li>
+            <li><span>Developer.</span></li>
+            <li><span>Front-end.</span></li>
+            <li><span>Back-end.</span></li>
         </ul>
         </div>
         </>
