6694586e6902c68343f48e2a956d02a83bea63ed
Paolollch <paolo.llamoza@lilab.pe>
2022-05-30 17:45:23 -0500

    Agregando routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar/Navbar';
-import Slider from './components/Slider/Slider';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
@@ -10,11 +10,9 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path='/' exact/>
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </Router>
-    <Slider>
-    </Slider>
     </>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../../src/App.css"
+import Slider from "../../components/Slider/Slider"
+
+function Home(){
+
+    return(
+    <>
+        <Slider/>
+    </>
+    );
+};
+
+export default Home;
